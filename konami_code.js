359e8f71f624575fefb664646c75384294604976
Andrew Parrucci <am.parrucci@gmail.com>
2019-01-07 23:27:34 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   
-  let incex = 0;
+  let index = 0;
   // Write your JavaScript code inside the init() function
 document.body.addEventListener ("keydown", function(e) {
   const key = e.detail || e.which;
